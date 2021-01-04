eb2a8fceca0db8b76b6c84f7cd5487ff287e3c42
Joshua Hazlewood <joshhaz2012@gmail.com>
2021-01-04 17:13:03 +0000

    Initial commit. Added Navbar and routing for main pages

---
+++ src/pages/coffee.js
@@ -0,0 +1,7 @@
+import { Typography } from "@material-ui/core";
+
+export function Coffee() {
+  return (
+    <Typography variant="h1">Coffee</Typography>
+  );
+}
---
+++ src/pages/espresso.js
@@ -0,0 +1,7 @@
+import { Typography } from "@material-ui/core";
+
+export function Espresso() {
+  return (
+    <Typography variant="h1">Espresso</Typography>
+  );
+}
